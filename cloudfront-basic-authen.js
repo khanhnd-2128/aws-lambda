58adadacb8953b74cba1544b9807438fd465eb05
root <root@b122128-lt.sun-asterisk.com>
2020-02-19 09:06:05 +0700

    lambda function for basic authen

---
+++ cloudfront-basic-authen.js
@@ -0,0 +1,34 @@
+// Deploy on region N.Virginia(us-east-1)
+// This code using nodejs 10.x on lmabda function.
+// Publish version for this lambda function and add trigger by cloudfront viewer request.
+'use strict';
+exports.handler = (event, context, callback) => {
+
+    // Get request and request headers
+    const request = event.Records[0].cf.request;
+    const headers = request.headers;
+
+    // Configure authentication
+    const authUser = 'khanhnd';
+    const authPass = 'toiLaKhaNh';
+
+    // Construct the Basic Auth string
+    const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
+
+    // Require Basic authentication
+    if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != authString) {
+        const body = 'Unauthorized';
+        const response = {
+            status: '401',
+            statusDescription: 'Unauthorized',
+            body: body,
+            headers: {
+                'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
+            },
+        };
+        callback(null, response);
+    }
+
+    // Continue request processing if authentication passed
+    callback(null, request);
+};
